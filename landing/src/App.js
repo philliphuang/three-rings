960022dbe6d73eb515d23d0ed4e3ff75f7ec529f
Phillip Huang <thephilliphuang@gmail.com>
2022-08-12 08:38:24 -0700

    Three rings version, no chinese yet

--- landing/src/App.js
+++ landing/src/App.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { SwitchTransition } from 'react-transition-group';
 import Fade from '@mui/material/Fade';
-import LightbulbCircleTwoToneIcon from '@mui/icons-material/LightbulbCircleTwoTone';
+import AnimationTwoToneIcon from '@mui/icons-material/AnimationTwoTone';
 import { useTheme } from '@mui/material/styles';
 
 function App() {
@@ -26,26 +26,26 @@ function App() {
       </Box>
 
       <Box sx={{ display: "flex", justifyContent: "center", p: 8}}>
-        <LightbulbCircleTwoToneIcon fontSize="large" sx={{ color: theme.palette.grey[400]}}/>
+        <AnimationTwoToneIcon fontSize="large" sx={{ color: theme.palette.grey[400]}}/>
       </Box>
 
       <SwitchTransition>
         { 
           chinese ?  
-          <Fade key={chinese}>
+          <Fade key={chinese} timeout={500}>
             <div>
               <Typography variant="h4" align="center" sx={{pb:4}}>
                 中文
               </Typography>
               <Typography paragraph align="justify">
-                中文
-              </Typography>
-              <Typography paragraph align="justify">
-                中文
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non eros nulla. Proin sollicitudin diam ultricies, placerat turpis sed, sodales nisi. Fusce semper maximus velit, cursus accumsan orci. Praesent consectetur, libero vitae rutrum ultrices, massa mauris placerat lectus, quis pulvinar mauris nibh nec risus. Nulla tempor vestibulum nisl, eget aliquam urna scelerisque in.
               </Typography>
               <Divider sx={{my:4}}/>
+              <Typography paragraph align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non eros nulla. Proin sollicitudin diam ultricies, placerat turpis sed, sodales nisi. Fusce semper maximus velit, cursus accumsan orci. Praesent consectetur, libero vitae rutrum ultrices, massa mauris placerat lectus, quis pulvinar mauris nibh nec risus. Nulla tempor vestibulum nisl, eget aliquam urna scelerisque in.
+              </Typography>
               <Typography>
-                中文
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non eros nulla. Proin sollicitudin diam ultricies, placerat turpis sed, sodales nisi. Fusce semper maximus velit, cursus accumsan orci. Praesent consectetur, libero vitae rutrum ultrices, massa mauris placerat lectus, quis pulvinar mauris nibh nec risus. Nulla tempor vestibulum nisl, eget aliquam urna scelerisque in.
               </Typography>
               <Typography sx={{mt: 16, color: theme.palette.grey[400]}} variant="body2" align="center">
                 中文 {today.getFullYear()}
@@ -53,29 +53,29 @@ function App() {
             </div>
           </Fade>
           :
-          <Fade key={chinese}>
+          <Fade key={chinese} timeout={500}>
             <div>
               <Typography variant="h4" align="center" sx={{pb:4}}>
                 Follow in our footsteps.
               </Typography>
               <Typography paragraph align="justify">
-                Star Path offers college admissions guidance from recent graduates of exceptionally competitive undergraduate programs. We believe only those who experience and conquer the process themselves can give the best advice. Our mentors understand firsthand the qualities that top universities want and the practicalities of integrating those qualities into one's life.
+                Three Rings offers college admissions guidance from recent graduates of exceptionally competitive undergraduate programs. We believe only those who experience and conquer the process themselves can give the best advice. Our mentors understand firsthand the qualities that top universities want and the practicalities of integrating those qualities into one's life.
               </Typography>
               <Divider sx={{my:4}}/>
               <Typography paragraph align="justify">
                 Our program supports a small cohort of students individually selected by our mentors. New availability opens when students exit the program or mentors offer additional time slots.
               </Typography>
               <Typography>
-                Applications are currently open. To apply for mentorship please submit the following information to <Link underline="hover">applications@paladinmentors.com</Link> or via WeChat at <Link underline="hover">WeChatIDHere</Link>:
+                Applications are currently open. To apply for mentorship please submit the following information to <Link underline="hover">apply@threerings.college</Link> or via WeChat at <Link underline="hover">WeChatIDHere</Link>:
               </Typography>
               <ul>
                 <li>The student's name and year of school</li>
                 <li>The parent's name and contact information</li>
                 <li>A brief description of the student's relevant activities and performance (e.g. interests, classes, GPA, extracurriculars, test scores, etc.)</li>
-                <li>A brief description of the student's long term goals (e.g. target colleges, majors, life goals, etc.)</li>
+                <li>A brief description of the student's long term goals (e.g. target colleges, anticipated majors, life goals, etc.)</li>
               </ul>
               <Typography sx={{mt: 12, mb: 4, color: theme.palette.grey[400]}} variant="body2" align="center">
-                Star Path Mentorship Group {today.getFullYear()}
+                Three Rings College Admissions {today.getFullYear()}
               </Typography>
             </div>
           </Fade>
