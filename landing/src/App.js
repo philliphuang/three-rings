13dece0db890bf788170fc9de75cbcdc7b41a74c
Phillip Huang <thephilliphuang@gmail.com>
2022-09-22 10:38:44 -0700

    Removed accordions, zhuzh up chinese

--- landing/src/App.js
+++ landing/src/App.js
@@ -46,8 +46,12 @@ function App(props) {
         {
           chinese ?
           <Grid item xs={3}>
-            <Typography gutterBottom variant="h2" align="center">三环</Typography>
-            <Typography variant="h6" align="center">入名校传帮带</Typography>
+            <Typography gutterBottom variant="h1" align="center" sx={{fontFamily: '"Ma Shan Zheng", serif'}}>三环</Typography>
+            <Typography variant="h4" align="center" sx={{
+              letterSpacing: "0.3em",
+              marginRight: "-0.3em", 
+              fontFamily: "serif",
+            }}>入名校传帮带</Typography>
           </Grid>  
           : 
           <Grid item xs={3}>
@@ -77,63 +81,60 @@ function App(props) {
             chinese ?  
             <Fade key={chinese} timeout={500}>
               <div>
-              <Typography variant="h4" align="center" sx={{pb:10}}>
+              <Typography variant="h3" align="center" sx={{pb:10, fontFamily: '"Ma Shan Zheng", serif'}}>
                 跟随我们的脚步
               </Typography>
 
-              <Accordion elevation={0} sx={{py: 1, background:"none", '&:before': {display: 'none'}}} disableGutters>
-                <AccordionSummary>
-                <Typography align="center" sx={{width: "100%", letterSpacing: "0.3em", marginRight: "-0.3em",}}>
-                  原理
-                </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography paragraph>
-                    三环 (Three Rings) 为竞争异常激烈的大学录取提供指导。根据我们自身被名校录取的经验, 我们能够提供最好的建议。
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                原理
+              </Typography>
+              <Typography paragraph>
+                为什么看似完美大学申请的学生仍然被拒绝？ 大学希望学生脱颖而出，但大学顾问专注于其他人已经拥有的东西：成绩和考试； 俱乐部和活动； 论文写作和暑期课程。
+              </Typography>
+              <Typography paragraph>
+                三环不一样。 作为哈佛和伯克利的新毕业生，我们知道在顶尖学校中脱颖而出的秘诀：激情。 只有真正的热情才能驱使学生实现其他人甚至无法想象的事情。
+              </Typography>
+              <Typography paragraph>
+                多年探索自己的激情让我们引导他人找到他们的激情。 我们首先与学生建立深厚的兄弟姐妹般的联系，以帮助他们了解自己并找到他们真正喜欢的专业。 然后，我们利用我们的经验将他们的专业转化为大学想要的世界级成就。
+              </Typography>
 
-              <Accordion elevation={0} sx={{py: 1, background:"none", '&:before': {display: 'none'}}} disableGutters>
-                <AccordionSummary>
-                <Typography align="center" sx={{width: "100%", letterSpacing: "0.3em", marginRight: "-0.3em",}}>
-                  导师
-                </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    导师 1: 哈佛 2018 毕业
-                  </Typography>
-                  <Typography paragraph>
-                    SAT 满分, GPA 4.0, 写作比赛全州夺冠, 为其所在高中十年来首次被哈佛大学录取的学生。华裔男生。会英语和国语。
-                  </Typography>
-                  <Typography>
-                    导师 2: 伯克利 2018 毕业
-                  </Typography>
-                  <Typography paragraph>
-                    计算机科学与经济学双学位, Monta Vista 高中毕业生, 被所有 UC 录取。华裔女生。会英语和国语。
-                  </Typography>
-                  <Typography>
-                    导师 3: 哈佛 2018 毕业
-                  </Typography>
-                  <Typography paragraph>
-                    世界顶尖科技公司主管工程师, 7 年辅导经验, 8 艺术媒介, 5 乐器, 6 运动。华裔男生。
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+              <Box  
+                sx={{
+                  border: 0,
+                  borderRadius: 0,
+                  p: 4,
+                  my: 8,
+                  background: theme.palette.grey[200],
+                }}
+              >
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                导师一
+              </Typography>
+              <Typography paragraph>
+                哈佛 2018 毕业, SAT 满分, GPA 4.0, 写作比赛全州夺冠, 为其所在高中十年来首次被哈佛大学录取的学生。华裔男生。会英语和国语。
+              </Typography>
+              <Divider sx={{my:4}}/>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                导师二
+              </Typography>
+              <Typography paragraph>
+                伯克利 2018 毕业, 计算机科学与经济学双学位, Monta Vista 高中毕业生, 被所有 UC 录取。华裔女生。会英语和国语。
+              </Typography>
+              <Divider sx={{my:4}}/>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                导师三
+              </Typography>
+              <Typography paragraph>
+                哈佛 2018 毕业, 世界顶尖科技公司主管工程师, 7 年辅导经验, 8 艺术媒介, 5 乐器, 6 运动。华裔男生。
+              </Typography>
+              </Box>
 
-              <Accordion elevation={0} sx={{py: 1, background:"none", '&:before': {display: 'none'}}} disableGutters>
-                <AccordionSummary>
-                <Typography align="center" sx={{width: "100%", letterSpacing: "0.3em", marginRight: "-0.3em",}}>
-                  申请
-                </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography paragraph>
-                    我们的计划仅针对导师们精心挑选的一部分学生。 如感兴趣，请发信息至 <Link underline="hover">apply@threeringsmentors.com</Link>。
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                申请
+              </Typography>
+              <Typography paragraph>
+                我们的计划仅针对导师们精心挑选的一部分学生。 如感兴趣，请发信息至 <Link underline="hover">apply@threeringsmentors.com</Link>。
+              </Typography>
 
               <Typography sx={{mt: 12, mb: 4, color: theme.palette.grey[400]}} variant="body2" align="center">
                 三环传帮带 {today.getFullYear()}
@@ -147,66 +148,58 @@ function App(props) {
                   Follow in our footsteps.
               </Typography>
 
-              <Accordion elevation={0} sx={{py: 1, background:"none", '&:before': {display: 'none'}}} disableGutters>
-                <AccordionSummary>
-                <Typography align="center" sx={{width: "100%", letterSpacing: "0.3em", marginRight: "-0.3em",}}>
-                  PHILOSOPHY
-                </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography paragraph>
-                    Why do students that seem to have the perfect college application still get rejected? Colleges want students that stand out, but college consultants focus on what everyone else already has: grades and tests; clubs and activities; essay writing and summer programs.
-                  </Typography>
-                  <Typography paragraph>
-                    Three Rings is different. As recent graduates from Harvard and Berkeley, we know the secret to standing out to top schools: passion. Only genuine passion drives a student to achieve things others never even imagine.
-                  </Typography>
-                  <Typography paragraph>
-                    Years of exploring our own passions let us guide others to find theirs. We first build a deep, sibling-like connection with students to help them understand themselves and find a specialty they truly enjoy. Then, we use our experience to transform their specialty into the world-class accomplishments that colleges want.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                PHILOSOPHY
+              </Typography>
+              <Typography paragraph>
+                Why do students that seem to have the perfect college application still get rejected? Colleges want students that stand out, but college consultants focus on what everyone else already has: grades and tests; clubs and activities; essay writing and summer programs.
+              </Typography>
+              <Typography paragraph>
+                Three Rings is different. As recent graduates from Harvard and Berkeley, we know the secret to standing out to top schools: passion. Only genuine passion drives a student to achieve things others never even imagine.
+              </Typography>
+              <Typography paragraph>
+                Years of exploring our own passions let us guide others to find theirs. We first build a deep, sibling-like connection with students to help them understand themselves and find a specialty they truly enjoy. Then, we use our experience to transform their specialty into the world-class accomplishments that colleges want.
+              </Typography>
 
-              <Accordion elevation={0} sx={{py: 1, background:"none", '&:before': {display: 'none'}}} disableGutters>
-                <AccordionSummary>
-                <Typography align="center" sx={{width: "100%", letterSpacing: "0.3em", marginRight: "-0.3em",}}>
-                  MENTORS
-                </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Mentor 1: Harvard Graduate 2018
-                  </Typography>
-                  <Typography paragraph>
-                    SAT Perfect Score, GPA 4.0, 1st place essay statewide, first Harvard admit in 10+ years from his high school. Chinese American male. Speaks English & Mandarin.
-                  </Typography>
-                  <Typography>
-                    Mentor 2: Berkeley Graduate 2018
-                  </Typography>
-                  <Typography paragraph>
-                    Computer Science and Economics double major, Monta Vista High graduate, accepted by all UCs. Chinese American female. Speaks English & Mandarin.
-                  </Typography>
-                  <Typography>
-                    Mentor 3: Harvard Graduate 2018
-                  </Typography>
-                  <Typography paragraph>
-                    Top tech company engineering lead, 7 years of tutoring experience, 8 art mediums, 5 musical instruments, 6 sports. Chinese American male.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+              <Box  
+                sx={{
+                  border: 0,
+                  borderRadius: 0,
+                  p: 4,
+                  my: 8,
+                  background: theme.palette.grey[200],
+                }}
+              >
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                MENTOR 1
+              </Typography>
+              <Typography paragraph>
+                Harvard Graduate 2018, SAT Perfect Score, GPA 4.0, 1st place essay statewide, first Harvard admit in 10+ years from his high school. Chinese American male. Speaks English & Mandarin.
+              </Typography>
+              <Divider sx={{my:4}}/>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                MENTOR 2
+              </Typography>
+              <Typography paragraph>
+                Berkeley Graduate 2018, Computer Science and Economics double major, Monta Vista High graduate, accepted by all UCs. Chinese American female. Speaks English & Mandarin.
+              </Typography>
+              <Divider sx={{my:4}}/>
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                MENTOR 3
+              </Typography>
+              <Typography paragraph>
+                Harvard Graduate 2018, Top tech company engineering lead, 7 years of tutoring experience, 8 art mediums, 5 musical instruments, 6 sports. Chinese American male.
+              </Typography>
+              </Box>
 
-              <Accordion elevation={0} sx={{py: 1, background:"none", '&:before': {display: 'none'}}} disableGutters>
-                <AccordionSummary>
-                  <Typography align="center" sx={{width: "100%", letterSpacing: "0.3em", marginRight: "-0.3em",}}>
-                    APPLICATIONS
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography paragraph>
-                    Due to limited availability, we individually select a small cohort of students to participate in our program.
-                    To apply for a mentor, please email <Link underline="hover">apply@threeringsmentors.com</Link>.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
+
+              <Typography align="center" sx={{pb: 2, letterSpacing: "0.3em", marginRight: "-0.3em",}}>
+                APPLICATIONS
+              </Typography>
+              <Typography paragraph>
+                Due to limited availability, we individually select a small cohort of students to participate in our program.
+                To apply for a mentor, please email <Link underline="hover">apply@threeringsmentors.com</Link>.
+              </Typography>
 
               <Typography sx={{mt: 12, mb: 4, color: theme.palette.grey[400]}} variant="body2" align="center">
                 Three Rings Mentors {today.getFullYear()}
